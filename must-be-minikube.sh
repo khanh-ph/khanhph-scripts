45fae30330c5c9597b866c2ada713273042d4c3a
Khanh Pham <86582199+khanh-ph@users.noreply.github.com>
2024-05-27 15:25:18 +0700

    Update must-be-minikube.sh

--- must-be-minikube.sh
+++ must-be-minikube.sh
@@ -1,3 +1,4 @@
 supported_context="minikube"
 current_context=$(kubectl config current-context 2> /dev/null | sed -e 's/[^@]*@//' | sed -e 's/.*/&/')
 [[ ${current_context} != ${supported_context} ]] && echo "WARN: Current context is not \""${supported_context}"\". Exiting." && exit 1
+exit 0
